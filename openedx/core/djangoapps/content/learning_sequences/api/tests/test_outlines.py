b3b321205cf65c6e04812af1678b0fc5f61c9bd7
David Ormsbee <dormsbee@edx.org>
2020-05-20 20:39:21 -0400

    Make Opaque Key identifiers in DB CASE SENSITIVE

--- openedx/core/djangoapps/content/learning_sequences/api/tests/test_outlines.py
+++ openedx/core/djangoapps/content/learning_sequences/api/tests/test_outlines.py
@@ -1,10 +1,12 @@
 """
-Top level API tests. Tests API public contracts only.
+Top level API tests. Tests API public contracts only. Do not import/create/mock
+models for this app.
 """
 from datetime import datetime, timezone
 
 from opaque_keys.edx.keys import CourseKey, UsageKey
 from opaque_keys.edx.locator import BlockUsageLocator
+import attr
 
 from openedx.core.djangolib.testing.utils import CacheIsolationTestCase
 from ..data import (
@@ -16,10 +18,9 @@ from ..outlines import (
 )
 
 
-class RoundTripTestCase(CacheIsolationTestCase):
+class CourseOutlineTestCase(CacheIsolationTestCase):
     """
-    Simple tests to ensure that we can pull back the same data we put in, and
-    that we don't break when storing or retrieving edge cases.
+    Simple tests around reading and writing CourseOutlineData. No user info.
     """
     @classmethod
     def setUpClass(cls):
@@ -56,6 +57,17 @@ class RoundTripTestCase(CacheIsolationTestCase):
         )
         cls.course_key = course_key
 
+    def test_case_sensitivity(self):
+        replace_course_outline(self.course_outline)
+        with self.assertRaises(CourseOutlineData.DoesNotExist):
+            ucase_course_key = CourseKey.from_string("course-v1:OPENEDX+LEARN+ROUNDTRIP")
+            outline = get_course_outline(ucase_course_key)
+
+    def test_deprecated_course_key(self):
+        old_course_key = CourseKey.from_string("Org/Course/Run")
+        with self.assertRaises(ValueError):
+            outline = get_course_outline(old_course_key)
+
     def test_simple_roundtrip(self):
         # Make sure it wasn't there in the beginning...
         with self.assertRaises(CourseOutlineData.DoesNotExist):
@@ -65,5 +77,44 @@ class RoundTripTestCase(CacheIsolationTestCase):
         outline = get_course_outline(self.course_key)
         assert outline == self.course_outline
 
-    # Add tests with more permutations of data, updates with same structure,
-    # updates with slightly different structure.
+    def test_empty_course(self):
+        empty_outline = attr.evolve(self.course_outline, sections=[])
+        self.assertFalse(empty_outline.sections)
+        self.assertFalse(empty_outline.sequences)
+        replace_course_outline(empty_outline)
+        assert empty_outline == get_course_outline(self.course_key)
+
+    def test_cached_response(self):
+        # First lets seed the data...
+        replace_course_outline(self.course_outline)
+
+        # Uncached access always makes three database checks: LearningContext,
+        # CourseSection, and CourseSectionSequence.
+        with self.assertNumQueries(3):
+            uncached_outline = get_course_outline(self.course_key)
+            assert uncached_outline == self.course_outline
+
+        # Successful cache access only makes a query to LearningContext to check
+        # the current published version. That way we know that values are never
+        # stale.
+        with self.assertNumQueries(1):
+            cached_outline = get_course_outline(self.course_key)
+
+        # Cache hits in the same process are literally the same object.
+        assert cached_outline is uncached_outline
+
+        # Now we put a new version into the cache...
+        new_version_outline = attr.evolve(
+            self.course_outline, published_version="2222222222222222"
+        )
+        replace_course_outline(new_version_outline)
+
+        # Make sure this new outline is returned instead of the previously
+        # cached one.
+        with self.assertNumQueries(3):
+            uncached_new_version_outline = get_course_outline(self.course_key)
+            assert new_version_outline == new_version_outline
+
+
+class UserCourseOutlineTestCase(CacheIsolationTestCase):
+    pass
